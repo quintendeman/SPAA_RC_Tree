6970e9f770f0e8255e2a1227c3f1e99af4867a0c
Humza <humzai@aware.aladdin.cs.cmu.edu>
2025-02-19 23:27:22 -0500

    plotting better

--- RCtrees/collect_data.sh
+++ RCtrees/collect_data.sh
@@ -5,10 +5,9 @@ echo "This script collects data for static and dynamic tree generation. Please s
 echo "num_threads,graph_size,ln,mean,dist,static_gen_time,static_tern_time,dynamic_gen_time,dynamic_tern_time,randomize" > output.csv
 
 
-CONST_GRAPH_SIZE=750 # graph size to use when running experiment with varying number of threads
+CONST_GRAPH_SIZE=10000000 # graph size to use when running experiment with varying number of threads
 
-graph_sizes=(500 600 700 800)
-# graph_sizes=(1000 10000 100000 1000000 10000000 50000000 100000000)
+graph_sizes=(1000 10000 100000 1000000 10000000 50000000 75000000 100000000)
 NUM_THREADS=(1 2 4 8 16 32)
 
 # run for ln 0.1 and mean 20 with exponential distribution
