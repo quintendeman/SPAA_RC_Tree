aaaa1b7ccfe4f41458d4e41258254924f97e9d30
Andrew Brady <acbrady@andrew.cmu.edu>
2025-02-22 10:55:05 -0500

    updated shell file

--- RCtrees/lcatest.sh
+++ RCtrees/lcatest.sh
@@ -1,6 +1,6 @@
-export myfile=outfile17.txt
-export myerrorfile=outfile17err.txt
-export PARLAY_NUM_THREADS=33
+export myfile=outfile18.txt
+export myerrorfile=outfile18err.txt
+export PARLAY_NUM_THREADS=50
 echo $PARLAY_NUM_THREADS > $myfile
 make lca.out >> $myfile
-nohup ./lca.out -forr -1 -pseed 74 >> $myfile 2> $myerrorfile  #redirect stderr to stdout? 
+nohup ./lca.out -forr -1 -pseed 80 >> $myfile 2> $myerrorfile  #redirect stderr to stdout? 
