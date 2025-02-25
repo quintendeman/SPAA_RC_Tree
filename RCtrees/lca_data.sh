820bd1283a88be39eb5001d214658e714b67ff0d
Andrew Brady <acbrady@aware.aladdin.cs.cmu.edu>
2025-02-25 10:05:08 -0500

    merged

--- RCtrees/lca_data.sh
+++ RCtrees/lca_data.sh
@@ -16,7 +16,6 @@ for PNT in "${NUM_THREADS[@]}"; do
     LD_PRELOAD=$jemellocpath PARLAY_NUM_THREADS=$PNT ./lca.out -forr -5 -n 10000000 -k 500000 -trials 10 -mean 20 -ln .1 >> output_lca.csv
 done
 
-
 LD_PRELOAD=$jemellocpath PARLAY_NUM_THREADS=32 ./lca.out -forr -4 -trials 10 -n 10000000 -k 10000000 -mean 40 -ln .9 >> output_lca.csv
 
 for PNT in "${NUM_THREADS[@]}"; do
@@ -24,3 +23,4 @@ for PNT in "${NUM_THREADS[@]}"; do
 done
 
 date
+
