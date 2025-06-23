06c6c33b9e47153118bd1fee81d98e78dc8a15b4
Humza <humzai@aware.aladdin.cs.cmu.edu>
2025-06-23 19:43:31 -0400

    nodes

--- RCtrees/collect_data_file_2.sh
+++ RCtrees/collect_data_file_2.sh
@@ -29,3 +29,35 @@ PARLAY_NUM_THREADS=48 ./testMSTfromFile.out USA-road-d.USA.gr 100 >> $filename
 PARLAY_NUM_THREADS=48 ./testMSTfromFile.out USA-road-d.USA.gr 10000 >> $filename
 PARLAY_NUM_THREADS=48 ./testMSTfromFile.out USA-road-d.USA.gr 100000 >> $filename
 PARLAY_NUM_THREADS=48 ./testMSTfromFile.out USA-road-d.USA.gr 1000000 >> $filename
+
+filename=output_mst_from_file_three.csv
+
+PARLAY_NUM_THREADS=1 ./testMSTfromFile2.out USA-road-d.USA.gr 1 >> $filename
+PARLAY_NUM_THREADS=1 ./testMSTfromFile2.out USA-road-d.USA.gr 100 >> $filename
+PARLAY_NUM_THREADS=1 ./testMSTfromFile2.out USA-road-d.USA.gr 10000 >> $filename
+PARLAY_NUM_THREADS=1 ./testMSTfromFile2.out USA-road-d.USA.gr 100000 >> $filename
+PARLAY_NUM_THREADS=1 ./testMSTfromFile2.out USA-road-d.USA.gr 1000000 >> $filename
+
+PARLAY_NUM_THREADS=8 ./testMSTfromFile2.out USA-road-d.USA.gr 1 >> $filename
+PARLAY_NUM_THREADS=8 ./testMSTfromFile2.out USA-road-d.USA.gr 100 >> $filename
+PARLAY_NUM_THREADS=8 ./testMSTfromFile2.out USA-road-d.USA.gr 10000 >> $filename
+PARLAY_NUM_THREADS=8 ./testMSTfromFile2.out USA-road-d.USA.gr 100000 >> $filename
+PARLAY_NUM_THREADS=8 ./testMSTfromFile2.out USA-road-d.USA.gr 1000000 >> $filename
+
+PARLAY_NUM_THREADS=16 ./testMSTfromFile2.out USA-road-d.USA.gr 1 >> $filename
+PARLAY_NUM_THREADS=16 ./testMSTfromFile2.out USA-road-d.USA.gr 100 >> $filename
+PARLAY_NUM_THREADS=16 ./testMSTfromFile2.out USA-road-d.USA.gr 10000 >> $filename
+PARLAY_NUM_THREADS=16 ./testMSTfromFile2.out USA-road-d.USA.gr 100000 >> $filename
+PARLAY_NUM_THREADS=16 ./testMSTfromFile2.out USA-road-d.USA.gr 1000000 >> $filename
+
+PARLAY_NUM_THREADS=32 ./testMSTfromFile2.out USA-road-d.USA.gr 1 >> $filename
+PARLAY_NUM_THREADS=32 ./testMSTfromFile2.out USA-road-d.USA.gr 100 >> $filename
+PARLAY_NUM_THREADS=32 ./testMSTfromFile2.out USA-road-d.USA.gr 10000 >> $filename2
+PARLAY_NUM_THREADS=32 ./testMSTfromFile2.out USA-road-d.USA.gr 100000 >> $filename
+PARLAY_NUM_THREADS=32 ./testMSTfromFile2.out USA-road-d.USA.gr 1000000 >> $filename
+
+PARLAY_NUM_THREADS=48 ./testMSTfromFile2.out USA-road-d.USA.gr 1 >> $filename
+PARLAY_NUM_THREADS=48 ./testMSTfromFile2.out USA-road-d.USA.gr 100 >> $filename
+PARLAY_NUM_THREADS=48 ./testMSTfromFile2.out USA-road-d.USA.gr 10000 >> $filename
+PARLAY_NUM_THREADS=48 ./testMSTfromFile2.out USA-road-d.USA.gr 100000 >> $filename
+PARLAY_NUM_THREADS=48 ./testMSTfromFile2.out USA-road-d.USA.gr 1000000 >> $filename
